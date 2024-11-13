eff04af7af112604d7096022c36f52cdefacffff
Lucass97 <luca.gregori97@gmail.com>
2024-11-13 21:59:04 +0100

    feat: add sensor_registry_armellini data and update upload-data script.

--- data/upload-data.sh
+++ data/upload-data.sh
@@ -1,4 +1,10 @@
 #!bin/bash
-sudo docker cp  ./generated hadoop-namenode:/opt/hadoop/data/generated
-sudo docker exec -it hadoop-namenode hadoop fs -mkdir /generated
-sudo docker exec -it hadoop-namenode hadoop fs -put /opt/hadoop/data/generated/indoor_sensor_data.csv /generated/indoor_sensor_data.csv
+
+sudo docker exec -it hadoop-namenode mkdir /opt/hadoop/data
+sudo docker exec -it hadoop-namenode mkdir /opt/hadoop/data/iot-lorawan
+
+sudo docker cp ./generated hadoop-namenode:/opt/hadoop/data/iot-lorawan/generated
+sudo docker cp ./metadata hadoop-namenode:/opt/hadoop/data/iot-lorawan/metadata
+
+sudo docker exec -it hadoop-namenode hadoop fs -put /opt/hadoop/data/iot-lorawan/generated/indoor_sensor_data.csv /iot-lorawan/generated/indoor_sensor_data.csv
+sudo docker exec -it hadoop-namenode hadoop fs -put /opt/hadoop/data/iot-lorawan/metadata/sensor_registry_armellini.csv /iot-lorawan/metadata/sensor_registry_armellini.csv
