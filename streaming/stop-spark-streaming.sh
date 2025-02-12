23b855d4f4c4015c4cff2989c7638da6ca6b8276
Lucass97 <luca.gregori97@gmail.com>
2025-02-12 17:05:19 +0100

    feat: Update the start-spark.streaming.sh script to correctly stop the job. The script now identifies and uses the PID of start-spark-streaming.py to terminate it.

--- streaming/stop-spark-streaming.sh
+++ streaming/stop-spark-streaming.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-# Output colors
-GREEN="\033[0;32m"
-RED="\033[0;31m"
-YELLOW="\033[1;33m"
-NC="\033[0m"
+BASE_PATH="/opt/bitnami/spark/spark-streaming"
+SCRIPT_NAME_TO_STOP="start-spark-streaming.py"
+COMMON_FUNCTIONS_PATH="../scripts/spark_functions.sh"
 
-sudo docker exec -it spark-master rm -r /opt/bitnami/spark/misc
+source "${COMMON_FUNCTIONS_PATH}"
 
-sudo docker cp ./stop-spark-streaming.py spark-master:/opt/bitnami/spark/stop-spark-streaming.py
-sudo docker cp ./misc spark-master:/opt/bitnami/spark/misc
+# Function to stop a Spark job running inside a Docker container
+stop_spark_job() {
+    
+  local SCRIPT_NAME="$1"
+  local CONTAINER_NAME="spark-master"
 
-sudo docker exec -it spark-master spark-submit stop-spark-streaming.py
+  # Find the PID of the process inside the container
+  PID=$(sudo docker exec $CONTAINER_NAME ps aux | grep "$SCRIPT_NAME" | grep -v "grep" | awk '{print $2}')
+
+  # Check if the PID was found
+  if [ -z "$PID" ]; then
+    echo -e "${RED}Process $SCRIPT_NAME not found running in container $CONTAINER_NAME.${NC}"
+    return 1
+  else
+    echo -e "${GREEN}Found PID $PID for process $SCRIPT_NAME.${NC}"
+    # Kill the process
+    sudo docker exec $CONTAINER_NAME kill $PID
+    if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Process $SCRIPT_NAME (PID $PID) successfully terminated.${NC}"
+    else
+      echo -e "${RED}Error killing process $SCRIPT_NAME (PID $PID).${NC}"
+      return 1
+    fi
+  fi
+}
+
+# Main script
+
+echo -e "${YELLOW}Stopping Spark streaming script...${NC}"
+stop_spark_job "$SCRIPT_NAME_TO_STOP"
+clear_folder "$BASE_PATH"
